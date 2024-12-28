78933fea6a2dc1af8da37b4455e62c9bc66718ca
boazf <boaz@feldbaum.name>
2024-12-28 11:57:21 +0200

    Add project name

--- include/Common.h
+++ include/Common.h
@@ -4,19 +4,25 @@
 #include <Trace.h>
 #include <Lock.h>
 
-//#define DEBUG_SD
-// #define DEBUG_ETHERNET
-// #define DEBUG_TIME
-// #define DEBUG_SFT
-// #define DEBUG_CONFIG
-// #define DEBUG_HTTP_SERVER
-// #define DEBUG_RECOVERY_CONTROL
-// #define DEBUG_HISTORY
-// #define DEBUG_STATE_MACHINE
-// #define DEBUG_POWER
+#define DEBUG_SD
+#define DEBUG_ETHERNET
+#define DEBUG_TIME
+#define DEBUG_SFT
+#define DEBUG_CONFIG
+#define DEBUG_HTTP_SERVER
+#define DEBUG_RECOVERY_CONTROL
+#define DEBUG_HISTORY
+#define DEBUG_STATE_MACHINE
+#define DEBUG_POWER
 
 #define NELEMS(a) (sizeof(a)/sizeof(*a))
 #define MAX_PATH 128
+
+#ifdef USE_WIFI
+#define APP_NAME "IWG-WIFI"
+#else
+#define APP_NAME "IWG-Wired"
+#endif
 
 extern CriticalSection csSpi;
 
