616b69495833d328de690f713c531e4b025863fc
boazf <boaz@feldbaum.name>
2024-12-28 11:59:55 +0200

    comment out all debug switches

--- include/Common.h
+++ include/Common.h
@@ -4,16 +4,16 @@
 #include <Trace.h>
 #include <Lock.h>
 
-#define DEBUG_SD
-#define DEBUG_ETHERNET
-#define DEBUG_TIME
-#define DEBUG_SFT
-#define DEBUG_CONFIG
-#define DEBUG_HTTP_SERVER
-#define DEBUG_RECOVERY_CONTROL
-#define DEBUG_HISTORY
-#define DEBUG_STATE_MACHINE
-#define DEBUG_POWER
+//#define DEBUG_SD
+// #define DEBUG_ETHERNET
+// #define DEBUG_TIME
+// #define DEBUG_SFT
+// #define DEBUG_CONFIG
+// #define DEBUG_HTTP_SERVER
+// #define DEBUG_RECOVERY_CONTROL
+// #define DEBUG_HISTORY
+// #define DEBUG_STATE_MACHINE
+// #define DEBUG_POWER
 
 #define NELEMS(a) (sizeof(a)/sizeof(*a))
 #define MAX_PATH 128
