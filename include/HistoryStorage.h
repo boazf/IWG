c6a73d2f72155ec35543c5a8aec42d98aa44816e
boazf <boaz@feldbaum.name>
2025-07-10 23:13:29 +0300

    Many comments and some small fixes

--- include/HistoryStorage.h
+++ include/HistoryStorage.h
@@ -35,17 +35,22 @@
     X(RecoveryFailure)
 
 #define X(a) a,
+/// @brief RecoveryStatus enum class.
 enum class RecoveryStatus
 {
     RecoveryStatuses
 };
 #undef X
 
+// Forward declaration of HistoryStorage class
 class HistoryStorage;
 
+/// @brief HistoryStorageItem class.
+/// This class represents a single item in the history storage.
 class HistoryStorageItem
 {
 public:
+    /// @brief Default constructor for HistoryStorageItem.
     HistoryStorageItem()
     {
         data.endTime = INT32_MAX;
@@ -56,6 +61,13 @@ public:
         data.startTime = INT32_MAX;
     }
 
+    /// @brief Constructor for HistoryStorageItem.
+    /// @param _recoverySource The recovery source (User Initiated or Auto Recovery)
+    /// @param _startTime The start time of the first recovery cycle.
+    /// @param _endTime The end time of the last recovery cycle. If the recovery is ongoing, this is set to INT32_MAX.
+    /// @param _modemRecoveries Number of modem recovery cycles that were performed.
+    /// @param _routerRecoveries Number of router recovery cycles that were performed.
+    /// @param _recoveryStatus The status of the recovery (OnGoingRecovery, RecoverySuccess, or RecoveryFailure).
     HistoryStorageItem(
         RecoverySource _recoverySource, 
         time_t _startTime, 
@@ -72,12 +84,25 @@ public:
         data.startTime = _startTime;
     }
     
+    /// @brief Copy constructor for HistoryStorageItem.
+    /// @param source The source HistoryStorageItem to copy from.
     HistoryStorageItem(const HistoryStorageItem &source)
     {
         *this = source;
     }
     
+    /// @brief Comparison operator for HistoryStorageItem.
+    /// @param other The other HistoryStorageItem to compare with.
+    /// @return True if the start time of the current item is greater than the other item's start time, false otherwise.
+    /// This operator is used to sort history items in ascending order based on their start time.
+    bool operator >(const HistoryStorageItem &other)
+    {
+        return data.startTime > other.data.startTime;
+    }
 
+    /// @brief Assignment operator for HistoryStorageItem.
+    /// @param source The source HistoryStorageItem to assign from.
+    /// @return A reference to the current HistoryStorageItem instance.
     HistoryStorageItem &operator=(const HistoryStorageItem& source)
     {
         data.endTime = source.data.endTime;
@@ -91,6 +116,8 @@ public:
     }
 
 private:
+    /// @brief Put the history item data into EEPROM at the specified index.
+    /// @param i The index in the EEPROM where the history item data should be stored.
     void put(int i) const
     {
 #ifdef DEBUG_HISTORY
@@ -106,13 +133,16 @@ private:
         Trace(", modem=");
         Trace(data.modemRecoveries);
         Trace(", start=");
-        Trace(data.startTime);
+        Trace((unsigned int)data.startTime);
         Trace(", end=");
-        Traceln(data.endTime);
+        Traceln((unsigned int)data.endTime);
 #endif
         EEPROM.put<HistoryStorageItemData>(HISTORY_EEPROM_START_ADDRESS + sizeof(int) + sizeof(HistoryStorageItemData) * i, data);
     }
 
+    /// @brief Get the history item data from EEPROM at the specified index.
+    /// @param i The index in the EEPROM from which the history item data should be retrieved.
+    /// @return A reference to the current HistoryStorageItem instance with the retrieved data.
     HistoryStorageItem &get(int i)
     {
         EEPROM.get<HistoryStorageItemData>(HISTORY_EEPROM_START_ADDRESS + sizeof(int) + sizeof(HistoryStorageItemData) * i, data);
@@ -129,26 +159,41 @@ private:
         Trace(", modem=");
         Trace(static_cast<int>(data.modemRecoveries));
         Trace(", start=");
-        Trace(data.startTime);
+        Trace((unsigned int)data.startTime);
         Trace(", end=");
-        Traceln(data.endTime);
+        Traceln((unsigned int)data.endTime);
 #endif
         return *this;
     }
 
 public:
+    /// @brief Get the recovery source of the history item.
+    /// @return A reference to the recovery source of the history item.
     RecoverySource &recoverySource() { return data.recoverySource; }
 
+    /// @brief Get the start time of the history item.
+    /// @return A reference to the start time of the history item.
     time_t & startTime() { return data.startTime; }
 
+    /// @brief Get the end time of the history item.
+    /// @return A reference to the end time of the history item.
     time_t &endTime() { return data.endTime; }
 
+    /// @brief Get the number of modem recoveries performed.
+    /// @return A reference to the number of modem recoveries.
     int &modemRecoveries() { return data.modemRecoveries; }
 
+    /// @brief Get the number of router recoveries performed.
+    /// @return A reference to the number of router recoveries.
     int &routerRecoveries() { return data.routerRecoveries; }
 
+    /// @brief Get the recovery status of the history item.
+    /// @return A reference to the recovery status of the history item.
     RecoveryStatus &recoveryStatus() { return data.recoveryStatus; }
 
+    /// @brief Get the recovery time of the history item.
+    /// @return The time of the last recovery event. If the recovery was successful, it
+    /// returns the end time of the recovery. If the recovery is ongoing or failed, it returns INT32_MAX.
     time_t recoveryTime() 
     { 
         if (data.recoveryStatus == RecoveryStatus::RecoverySuccess)
@@ -158,6 +203,8 @@ public:
     }
 
 private:
+    /// @brief Data structure to hold the history item data.
+    /// This structure is used to store the history item data in EEPROM.
     struct HistoryStorageItemData
     {
         RecoverySource recoverySource;
@@ -168,33 +215,70 @@ private:
         RecoveryStatus recoveryStatus;
     };
 
+    /// @brief The data of the history item.
+    /// This is the actual data that will be stored in EEPROM.
     HistoryStorageItemData data;
 
+    /// @brief Friend class declaration for HistoryStorage.
     friend class HistoryStorage;
 };
 
+/// @brief HistoryStorage class.
+/// This class is responsible for managing the history storage in EEPROM.
+/// The records in EEPROM are stored in a circular buffer fashion.
+/// It allows adding new history items, resizing the storage, and retrieving history items.
 class HistoryStorage
 {
 public:
     HistoryStorage();
 
+    /// @brief Initialize the history storage.
+    /// This function finds the latest recovery item in the cyclical history storage.
+    /// This location will be used later for adding new history items.
+    /// @param maxRecords The maximum number of history records to store.
     void init(int maxRecords);
+    /// @brief Add a history item to the storage.
+    /// @param item The history item to add to the storage.
+    /// The history items are added in a circular buffer fashion.
+    /// If the storage is full, the oldest item will be overwritten.
     void addHistory(HistoryStorageItem &item);
+    /// @brief Get the number of available history records.
+    /// @return The number of available history records in the storage.
     int available();
+    /// @brief Resize the history storage.
+    /// @param maxRecords The new maximum number of history records to store.
     void resize(int maxRecords);
+    /// @brief Get a history item by index.
+    /// @param index The index of the history item to retrieve.
     const HistoryStorageItem getItem(int index);
+    /// @brief Get the last recovery time.
+    /// @return The time of the last recovery event. If no recovery has occurred, it
+    /// returns INT32_MAX.
+    /// If the last recovery was successful, it returns the end time of the recovery.
     time_t getLastRecovery();
 
 private:
+    /// @brief The maximum number of history records to store.
     int maxRecords;
+    /// @brief The number of available history records in the storage.
     int availableRecords;
+    /// @brief The index of the start of the history records in the storage.
+    /// This is the index where the next history item will be added.
     int startIndex;
+    /// @brief The last recovery time.
+    /// This is the time of the last recovery event. If no recovery has occurred, it
+    /// is set to INT32_MAX. 
+    /// If the last recovery was successful, it is set to the end time of the recovery.
     time_t lastRecovery;
 
 private:
+    /// @brief Store the number of available records in EEPROM.
     void putAvailableRecords();
+    /// @brief Retrieve the number of available records from EEPROM.
     void getAvailableRecords();
 #ifdef DEBUG_HISTORY
+    /// @brief Report the initialization result of the history storage.
+    /// This function is used for debugging purposes to report the initialization result of the history storage.
     void ReportInitializationResult();
 #endif
 };
--- src/HistoryStorage.cpp
+++ src/HistoryStorage.cpp
@@ -42,7 +42,7 @@ void HistoryStorage::ReportInitializationResult()
     tm tr;
     char buff[64];
     localtime_r(&lastRecovery, &tr);
-    strftime(buff, sizeof(buff), "%d/%m/%Y %T", &tr);
+    strftime(buff, sizeof(buff), "%d/%m/%Y %H:%M:%S", &tr);
     Trace("Last Recovery: ");
     Traceln(buff);
 }
@@ -56,7 +56,7 @@ void HistoryStorage::init(int _maxRecords)
     EEPROM.commit();
 #endif
     maxRecords = _maxRecords;
-    getAvailableRecords();
+    getAvailableRecords(); // Retrieve the number of available records from EEPROM
     if (availableRecords == -1)
     {
         // Reset history on first time the board is running
@@ -65,6 +65,8 @@ void HistoryStorage::init(int _maxRecords)
     }
     if (availableRecords < maxRecords)
     {
+        // If the number of available records is less than the maximum records, set startIndex to 
+        // the number of available records and set lastRecovery to the recovery time of the last record.
         startIndex = availableRecords;
         if (availableRecords > 0)
         {
@@ -73,6 +75,7 @@ void HistoryStorage::init(int _maxRecords)
             lastRecovery = historyStorageItem.recoveryTime();
         }
         else
+            // If there are no available records, set lastRecovery to INT32_MAX.
             lastRecovery = INT32_MAX;
 #ifdef DEBUG_HISTORY
         ReportInitializationResult();
@@ -80,6 +83,9 @@ void HistoryStorage::init(int _maxRecords)
         return;
     }
 
+    // If the number of available records is equal to the maximum records, find the earliest record
+    // and set startIndex to the index of that record.
+    // The lastRecovery to the recovery time of the oldest record.
     time_t minT = INT32_MAX;
     startIndex = 0;
     bool first = true;
@@ -92,6 +98,11 @@ void HistoryStorage::init(int _maxRecords)
         time_t startTime = historyStorageItem.startTime();
         if (minT > startTime)
         {
+            // We always get here with record index 0.
+            // Next time that we'll get here, we will actually hit the earliest record.
+            // This is because we are using a circular buffer.
+            // If don't get here again, this means that all records are arranged in ascending order
+            // and the first record is the earliest one.
             minT = startTime;
             startIndex = i;
             if (!first)
@@ -108,17 +119,19 @@ void HistoryStorage::init(int _maxRecords)
 
 void HistoryStorage::addHistory(HistoryStorageItem &item)
 {
+    // Store the item in the EEPROM at the current startIndex.
     item.put(startIndex);
-    if (startIndex == availableRecords && availableRecords < maxRecords)
+    if (availableRecords < maxRecords)
     {
+        // If the circular buffer is not full yet, increment the availableRecords count.
         availableRecords++;
         putAvailableRecords();
     }
+    // Increment the startIndex in a circular manner.
     startIndex = (startIndex + 1) % maxRecords;
-    if (item.recoveryStatus() != RecoveryStatus::RecoverySuccess)
-        lastRecovery = INT32_MAX;
-    else
-        lastRecovery = item.endTime();
+    // Update last recovery time.
+    lastRecovery = item.recoveryTime();
+    // Commit changes to EEPROM
     EEPROM.commit();
 }
 
@@ -129,17 +142,23 @@ void HistoryStorage::resize(int _maxRecords)
 
     if (availableRecords < maxRecords)
     {
+        // We haven't reached the maximum records yet, so we can just set the new maxRecords.
         if (availableRecords <= _maxRecords)
         {
+            // If the new maxRecords is greater than or equal to the available records,
+            // we can just set maxRecords to _maxRecords and return.
             maxRecords = _maxRecords;
             return;
         }
         else
         {
+            // If the new maxRecords is less than the available records, we need to reshuffle the records.
             maxRecords = availableRecords;
         }
     }
 
+    // Calculate the difference between the current maxRecords and the new _maxRecords.
+    // If the difference is zero, we don't need to do anything.
     int delta = _maxRecords - maxRecords;
     if (delta == 0)
         return;
@@ -148,26 +167,70 @@ void HistoryStorage::resize(int _maxRecords)
 
     if (delta < 0)
     {
-        if (startIndex - delta <= maxRecords)
+        // If we are reducing the number of records, we need to shift the records.
+        if (startIndex <= _maxRecords)
         {
-
-            for(int i = startIndex; i < maxRecords + delta; i++)
+            // maxRecords = 10
+            // startIndex = 5
+            // availableRecords = 10
+            // _maxRecords = 6
+            // delta = -4
+            // +-+-+-+-+-+-+-+-+-+-+
+            // |5|6|7|8|9|0|1|2|3|4|
+            // +-+-+-+-+-+-+-+-+-+-+
+            //            ↑
+            // startIndex ┘
+            // Shift from the end of the buffer to the startIndex.
+            for(int i = startIndex; i < _maxRecords; i++)
                 item.get(i - delta).put(i);
             maxRecords = _maxRecords;
             if (startIndex >= maxRecords)
                 startIndex = 0;
+            // +-+-+-+-+-+-+
+            // |5|6|7|8|9|4|
+            // +-+-+-+-+-+-+
+            //            ↑
+            // startIndex ┘
         }
         else
         {
+            // maxRecords = 10
+            // startIndex = 7
+            // availableRecords = 10
+            // _maxRecords = 6
+            // delta = -4
+            // +-+-+-+-+-+-+-+-+-+-+
+            // |3|4|5|6|7|8|9|0|1|2|
+            // +-+-+-+-+-+-+-+-+-+-+
+            //                ↑
+            //     startIndex ┘
+            // Shift the latest _maxRecords to the start of the buffer.
             maxRecords = _maxRecords;
             delta = startIndex - maxRecords;
             startIndex = 0;
             for (int i = 0; i < maxRecords; i++)
                 item.get(i + delta).put(i);
+            // maxRecords = 6
+            // delta = 7 - 6 = 1
+            // +-+-+-+-+-+-+
+            // |4|5|6|7|8|9|
+            // +-+-+-+-+-+-+
+            //  ↑
+            //  └─ startIndex
         }
     }
     else
     {
+        // maxRecords = 10
+        // startIndex = 7
+        // availableRecords = 10
+        // _maxRecords = 12
+        // delta = 2
+        // +-+-+-+-+-+-+-+-+-+-+
+        // |3|4|5|6|7|8|9|0|1|2|
+        // +-+-+-+-+-+-+-+-+-+-+
+        //                ↑
+        //     startIndex ┘
         // Bubble  sort
         for (int i = 0; i < maxRecords - 1; i++)
         {
@@ -177,8 +240,7 @@ void HistoryStorage::resize(int _maxRecords)
             for (int j = 0; j < maxRecords - i - 1; j++)
             {
                 HistoryStorageItem sj1;
-                sj1.get(j+1);
-                if (sj.startTime() > sj1.startTime())
+                if (sj > sj1.get(j+1))
                 {
                     sj.put(j+1);
                     sj1.put(j);
@@ -192,6 +254,14 @@ void HistoryStorage::resize(int _maxRecords)
         }
         maxRecords = _maxRecords;
         startIndex = availableRecords < maxRecords ? availableRecords : 0; 
+        // +-+-+-+-+-+-+-+-+-+-+-+-+
+        // |0|1|2|3|4|5|6|7|8|9| | |
+        // +-+-+-+-+-+-+-+-+-+-+-+-+
+        //                      ↑
+        //           startIndex ┘
+        // startIndex = 10
+        // maxRecords = 12
+        // availableRecords = 10
     }
 
     if (availableRecords > maxRecords)
