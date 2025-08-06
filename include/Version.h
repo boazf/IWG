86c2cd35f3048f41c78b849a94646012631e99a3
boazf <boaz@feldbaum.name>
2025-08-06 21:50:45 +0300

    Comments

--- include/Version.h
+++ include/Version.h
@@ -20,39 +20,74 @@
 #define VERSION_H
 #include <Arduino.h>
 
+/// @brief Version class provides methods to manage firmware versioning and OTA updates.
 class Version
 {
 public:
+    /// @brief Get the current installed firmware version.
+    /// @return A String representing the current installed firmware version.
     static String getCurrentVersion();
+    /// @brief Get the OTA version available for update.
+    /// @return A String representing the OTA version available for update, or "Unknown" if not available.
     static String getOtaVersion();
 
     typedef enum class _UpdateResult
     {
-        error,
-        noAvailUpdate,
-        done,
-        unknown
+        error,          ///< An error occurred during the update process.
+        noAvailUpdate,  ///< No available update was found.
+        done,           ///< The update process completed successfully.
+        unknown         ///< The update result is unknown.
     } UpdateResult;
 
+    /// @brief Update the firmware from the OTA server.
+    /// @return The result of the update process.
     static UpdateResult updateFirmware();
 
+    /// @brief Callback type for when the update process starts.
     typedef void (*OnStart)();
+    /// @brief Callback type for when the update process ends.
     typedef void (*OnEnd)();
-    typedef void (*OnProgress)(int, int);
-    typedef void (*OnError)(int, const String&);
+    /// @brief Callback type for reporting update progress.
+    /// @param sent The number of bytes sent so far.
+    /// @param total The total number of bytes to be sent.
+    typedef void (*OnProgress)(int sent, int total);
+    /// @brief Callback type for reporting errors during the update process.
+    /// @param error The error code.
+    /// @param message A String containing the error message.
+    typedef void (*OnError)(int error, const String& message);
 
+    /// @brief Set the callback for when the update process starts.
+    /// @param onStartCallback The callback function to be called when the update starts.
     static void onStart(OnStart onStartCallback);
+    /// @brief Set the callback for when the update process ends.
+    /// @param onEndCallback The callback function to be called when the update ends.
     static void onProgress(OnProgress onProgressCallback);
+    /// @brief Set the callback for reporting when the update process ends.
+    /// @param onEndCallback The callback function to be called when the update ends.
     static void onEnd(OnEnd onEndCallback);
+    /// @brief Set the callback for reporting errors during the update process.
+    /// @param onErrorCallback The callback function to be called when an error occurs.
     static void onError(OnError onErrorCallback);
     
 private:
+    /// @brief Get the base URL for the OTA drive.
+    /// @return A String containing the base URL for the OTA drive.
     static String getOtaDriveBaseUrl();
+    /// @brief Get the base parameters for the OTA update.
+    /// @return A String containing the base parameters for the OTA update.
     static String getBaseParams();
+    /// @brief Get the chip ID for the OTA update.
+    /// @return A String containing the chip ID for the OTA update.
     static String getChipId();
+    /// @brief  Get the update URL for the OTA update.
+    /// @return A String containing the update URL for the OTA update.
+    static String getUpdateURL();
 
 private:
+    /// @brief A string representing unknown version. this string is used when the version cannot be determined.
     static const String unknownVersion;
+    /// @brief The API key for the OTA update.
+    /// @note This key is used to authenticate the OTA update requests.
     static const char *apiKey;
 };
 
--- src/Version.cpp
+++ src/Version.cpp
@@ -30,6 +30,10 @@ const String Version::unknownVersion = "Unknown";
 #define API_KEY "d54ef031-6f61-4f00-bcdc-c803c3fa8ce8"
 #endif
 
+// The current version of the application.
+// This version is reported as the currently install version 
+// and also used for OTA updates.
+// This version should be updated with each release.
 #define APP_VERSION "1.0.27"
 
 const char *Version::apiKey = API_KEY;
@@ -41,30 +45,44 @@ String Version::getCurrentVersion()
 
 String Version::getOtaVersion()
 {
+    // Create an HTTP client to fetch the OTA version.
+    // The client will automatically stop when it goes out of scope.
     AutoStopClient client;
+    // Create an instance of HttpClientEx to handle the HTTP request.
     HttpClientEx http(client);
-    String url = getOtaDriveBaseUrl() + "update?" + getBaseParams();
-    http.begin(url);
+    // Set the http agent for the request.
+    http.begin(getUpdateURL());
+    // Perform a GET request to fetch the OTA version.
     http.get();
+    // Check the response status code.
+    // If the response code is less than 0, it indicates an error.
+    // If the response code is 304, it means no update is available, so return the current version.
     int code = http.responseStatusCode();
     if (code < 0)
         return unknownVersion;
     if (code == 304)
         return getCurrentVersion();
 
+    // Check the headers for the "X-Version" header to get the OTA version.
     HttpClientEx::Headers headers[] = {{"X-Version"}};
     http.collectHeaders(headers, NELEMS(headers));
 
     if (headers[0].value.isEmpty())
+        // If the "X-Version" header is not found or is empty, return the unknown version.
         return unknownVersion;
 
+    // If the "X-Version" header is found, return its value.
     return headers[0].value;
 }
 
 Version::UpdateResult Version::updateFirmware()
 {
+    // Create an HTTP client to fetch the OTA version.
+    // The client will automatically stop when it goes out of scope.
     AutoStopClient client;
-    HttpUpdateResult ret = httpUpdate.update(client, getOtaDriveBaseUrl() + "update?" + getBaseParams(), getCurrentVersion());
+    // Do the OTA update using the HttpUpdate class.
+    HttpUpdateResult ret = httpUpdate.update(client, getUpdateURL(), getCurrentVersion());
+    // Convert the HttpUpdateResult to Version::UpdateResult.
     switch(ret)
     {
         case HttpUpdateResult::HTTP_UPDATE_FAILED:
@@ -81,25 +99,32 @@ Version::UpdateResult Version::updateFirmware()
 
 void Version::onStart(OnStart onStartCallback)
 {
+    // Set the callback for when the update process starts.
     httpUpdate.onStart(onStartCallback);
 }
 
 void Version::onProgress(OnProgress onProgressCallback)
 {
+    // Set the callback for when the update process progresses.
     httpUpdate.onProgress(onProgressCallback);
 }
 
 void Version::onEnd(OnEnd onEndCallback)
 {
+    // Set the callback for when the update process ends.
     httpUpdate.onEnd(onEndCallback);
 }
 
 void Version::onError(OnError onErrorCallback)
 {
+    // Set the callback for when an error occurs during the update process.
     static OnError otaOnError;
     otaOnError = onErrorCallback;
+    // HttpUpdate error callback has no error message, only error code.
+    // However, we can use the getLastErrorString() method to get the error message.
     httpUpdate.onError([](int errCode)
     {
+        // Add the error message to the error callback.
         String errMessage = httpUpdate.getLastErrorString();
         otaOnError(errCode, errMessage);
     });
@@ -120,4 +145,9 @@ String Version::getChipId()
 String Version::getBaseParams()
 {
     return String("k=") + apiKey + "&v=" + getCurrentVersion() + "&s=" + getChipId();
+}
+
+String Version::getUpdateURL()
+{
+    return getOtaDriveBaseUrl() + "update?" + getBaseParams();
 }
