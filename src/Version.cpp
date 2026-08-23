a97c1683b7972520cbbdec8d149b9859dbd665ba
boazf <boaz@feldbaum.name>
2026-08-23 06:50:30 +0300

    bump version

--- src/Version.cpp
+++ src/Version.cpp
@@ -28,7 +28,7 @@ const String Version::unknownVersion = "Unknown";
 // This version is reported as the currently install version 
 // and also used for OTA updates.
 // This version should be updated with each release.
-#define APP_VERSION "1.0.30"
+#define APP_VERSION "1.0.31"
 
 String Version::getCurrentVersion()
 {
