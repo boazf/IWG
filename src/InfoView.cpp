a111db93a63a847dead3d41318e79bf4ba411d71
boazf <boaz@feldbaum.name>
2026-09-10 23:49:39 +0300

    Fix WiFi build

--- src/InfoView.cpp
+++ src/InfoView.cpp
@@ -15,8 +15,36 @@ ViewFiller InfoView::fillers[] =
     /*  0 */ [](String &fill){ fill = Version::getCurrentVersion(); },
     /*  1 */ [](String &fill){ fill = Version::getBuild(); },
     /*  2 */ [](String &fill){ fill = Eth.localIP().toString(); },
-    /*  3 */ [](String &fill){ byte macAddress[6]; Eth.MACAddress(macAddress); char macStr[18]; sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]); fill = macStr; },
+    /*  3 */ [](String &fill)
+    {
+        byte macAddress[6]; 
+#ifdef USE_WIFI
+        WiFi.macAddress(macAddress); 
+#else
+        Eth.MACAddress(macAddress); 
+#endif
+        char macStr[18]; 
+        sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]); 
+        fill = macStr; 
+    },
     /*  4 */ [](String &fill){ fill = String(t_now); },
+    /* 5 */ [](String &fill)
+    { 
+        fill = 
+#ifdef USE_WIFI
+            String("date.toLocaleString(navigator.language, {") +
+            "weekday: 'short'," +
+            "day: '2-digit'," +
+            "month: 'short'," +
+            "year: 'numeric'," +
+            "hour: '2-digit'," +
+            "minute: '2-digit'," +
+            "second: '2-digit'," +
+            "hour12: false})";
+#else
+            "date.toUTCString().replace(\" GMT\", \"\")";
+#endif
+    }
 };
 
 int InfoView::getFillers(const ViewFiller *&_fillers)
